984271620a42e36c34a3a5c2e24559e62e8eefcf
Stefan Eng <stefaneng13@gmail.com>
2020-10-22 16:43:39 -0700

    RSI lesion signal / PI-RADS comparison
    Various fixes in model evaluation to write report.

--- R/eval.models.R
+++ R/eval.models.R
@@ -84,6 +84,156 @@ threshold.summary.stats <- function(model, threshold) {
     do.call(rbind.data.frame, res)
 }
 
-optimal.threshold.summary <- function(models) {
+#' Computes F_beta score
+#'
+#' @param precision
+#' @param recall
+#' @param beta
+#'
+#' @return
+#' @export
+#'
+#' @examples
+F_beta <- function(precision, recall, beta = 1) {
+    unname(
+        (1 + beta^2) * (precision * recall) / (beta^2 * precision + recall)
+    )
+}
 
+# Combines the importance score of dummy variables into one
+squash.importance <- function(importance, FUN = 'sum') {
+    df <- importance
+    df$row.names <- row.names(df)
+    df$row.names <- gsub('(.*)\\..*', '\\1', df$row.names)
+
+    agg.df <- aggregate(df$Overall, by = list(vars = df$row.names), FUN=FUN)
+    res <-data.frame(Overall = agg.df[, 2])
+    rownames(res) <- agg.df[, 1]
+    res
+}
+
+#' Compute the variable importance for GBM, XGB, rpart
+#'
+#' @param x
+#' @param onlyImportance boolean indicating whether only a data frame with the importance scores be returned? FALSE returns class 'caret::varImp.train'.
+#' @param squash Should multiple dummy variables be squashed into one variable? (Relevant for xgb models)
+#'
+#' @return
+#' @export
+#'
+#' @examples
+var.imp <- function(x, onlyImportance = TRUE, squash = TRUE) {
+    stopifnot('train' == class(x));
+    if('gbm' == x$method) {
+        gbm.sum <- gbm::summary.gbm(x$finalModel, plotit = FALSE);
+        importance <- data.frame(Overall = gbm.sum[,2], row.names = gbm.sum[,1]);
+        res <- list(
+            model = 'gbm',
+            calledFrom = 'summary.gbm',
+            importance = importance
+        );
+        class(res) <- 'varImp.train';
+    } else {
+        res <- caret::varImp(x);
+    }
+    row.names(res$importance) <- gsub('(.*)\\.1', '\\1', row.names(res$importance));
+    if(squash) {
+        res$importance <- squash.importance(res$importance);
+    }
+    res$importance$variable = row.names(res$importance);
+    if(onlyImportance) res <- res$importance;
+    res
+}
+
+#' Combine the variable importance scores from multiple models into one data frame
+#'
+#' @param x list of caret train classes
+#'
+#' @return
+#' @export
+#'
+#' @examples
+var.imp.combine <-  function(x) {
+    res <- Reduce(function(...) merge(..., by = 'variable', all = TRUE), lapply(x, var.imp));
+    rownames(res) <- res$variable;
+    if(!is.null(names(x))) {
+        colnames(res) <- c('variable', names(x));
+    }
+
+    res
+}
+
+#' Compare variable importance across multiple models
+#'
+#' @param x
+#' @param ranks
+#'
+#' @return
+#' @export
+#'
+#' @examples
+compare.var.imp <- function(x, include.ranks = FALSE) {
+    model.varImp <- lapply(x, function(x) var.imp(x)$importance)
+    if(is.null(names(x))) {
+        names(x) <- unlist(lapply(x, `[[`, 'method'));
+    }
+
+    all.varnames <- unlist(lapply(model.varImp, rownames));
+    all.varnames <- unique(gsub('(.*)\\.1', '\\1', all.varnames));
+    var.importance <- data.frame(variable = all.varnames);
+    rownames(var.importance) <- all.varnames;
+    for(model.name in names(model.varImp)) {
+        imp.df <- model.varImp[[model.name]];
+        var.importance[[model.name]] <- NA;
+        var.importance[rownames(imp.df), model.name] <- imp.df$Overall;
+    }
+    if(include.ranks) {
+        ranks <- as.data.frame(lapply(var.importance[, -1] * -1, rank));
+        names(ranks) <- paste0(names(ranks), '.rank');
+        ranks$mean.rank <- rowMeans(ranks);
+        var.importance <- cbind.data.frame(var.importance, ranks);
+    }
+    var.importance;
+}
+
+#' Creates a summary data frame from the given (nested models)
+#' Uses Youden's J statistic to find optimal threshold
+#'
+#' @param models the models with first level = target and second level list the different models for that target
+#' @param models.roc optional ROC objects corresponding to the models
+#'
+#' @return
+#' @export
+#'
+#' @examples
+summarize.models <- function(models, models.roc = NULL) {
+    if(is.null(models.roc)) {
+        models.roc <- lapply(models, function(x) {
+            lapply(x, function(m) {
+                bestPreds <- with(m, merge(pred, bestTune));
+                pROC::roc(predictor = bestPreds$yes, response = bestPreds$obs, direction = '<', levels = c('no', 'yes'));
+            })
+        })
+    }
+
+    thresholds.info.list <- lapply(names(models), function(x) {
+        target.list <- models[[x]]
+
+        res <- lapply(names(target.list), function(y) {
+            m <- target.list[[y]]
+            m.roc <- models.roc[[x]][[y]]
+            threshold.roc <- as.numeric(coords(m.roc, 'best')[1])
+
+            thres.res <- colMeans(threshold.summary.stats(m, threshold.roc), na.rm = TRUE);
+            thres.res$model <- y;
+            thres.res$target <- x;
+            thres.res$threshold <- threshold.roc;
+
+            thres.res;
+        })
+
+        do.call(rbind.data.frame, res);
+    })
+
+    do.call(rbind.data.frame, thresholds.info.list);
 }
